3db88adec050587a8a8b476282073d97416c2258
abansal4 <abansal4@ncsu.edu>
2018-06-03 19:50:35 +0000

    Problem 2

--- main.cc
+++ main.cc
@@ -8,11 +8,13 @@ int main()
  int sum=1, n=0, temp=0, temp2=0;
  cout << "Enter the number of elements: " << endl;
  cin >> n;
- for(int i=0; i<n; i++)
+// for(int i=0; i<n; i++)
+ while(sum < 4000000)
  {
 	sum = sum + temp;
-	printf("\n %d ", temp);
+	printf("\n %d ", sum);
 	temp = sum-temp;
+	if(sum%2==0)	temp2 = temp2 + sum;
  }
-	//cout << "\nSUM=" << sum << endl;
+	cout << "\nRequired sum=" << temp2 << endl;
 }
